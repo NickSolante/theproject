e1393a74f2057dbdb01ae60c6412bb81ca5c93a4
frostnovus <psolante@deakin.edu.au>
2020-01-08 06:08:32 +1100

    users.js has prop checking

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import UserItem from './UserItem'
 import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
 const Users = ({ users, loading }) => {
   const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
+  }
+  Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
   }
   if (!loading) {
     return (
