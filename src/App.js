18f0d5c547d69c42bd9bf7da937f3f2672b28350
frostnovus <psolante@deakin.edu.au>
2020-01-09 20:31:28 +1100

    search passes back props

--- src/App.js
+++ src/App.js
@@ -20,12 +20,17 @@ class App extends Component {
     console.log(res.data)
     this.setState({ users: res.data, loading: false })
   }
+
+  //text is being passed back via searchusers sent from Search like emit in vue
+  searchUsers = text => {
+    console.log(text)
+  }
   render() {
     return (
       <div className='App'>
         <Navbar title='Github finder'></Navbar>
         <div className='container'>
-          <Search></Search>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users}></Users>
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 
-export class Search extends Component {
+class Search extends Component {
   state = {
     text: ''
+  }
+  onChange = e => {
+    //using e.target name allows you to access multiple inputs without having to have different onchange functions
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.searchUsers(this.state.text)
+    this.setState({ text: '' })
   }
   render() {
     return (
       <div>
-        <form className='form'>
+        <form onSubmit={this.onSubmit} className='form'>
           <input
             type='text'
             name='text'
             placeholder='Search Users...'
             value={this.state.text}
+            onChange={this.onChange}
           ></input>
           <input
             type='submit'
