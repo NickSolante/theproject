8d418e68a1cf9533ef0731bcede1f5b58c3007ee
NickSolante <41457938+NickSolante@users.noreply.github.com>
2020-01-16 18:25:34 +1100

    hireable function

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -25,7 +25,10 @@ class User extends Component {
       html_url,
       following,
       followers,
-      hireable
+      hireable,
+      company,
+      public_repos,
+      public_gists
     } = this.props.user
     const { loading } = this.props
     if (loading) return <Spinner></Spinner>
@@ -40,6 +43,59 @@ class User extends Component {
         ) : (
           <i className='fas fa-times-circle text-danger' />
         )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt=''
+              className='round-img'
+              style={{ width: '150px' }}
+            ></img>
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              {' '}
+              Visit github profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username : {login}</strong>
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Blog : {blog}</strong>
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company : {company}</strong>
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'> Followers: {followers}</div>
+          <div className='badge badge-primary'> Following: {following}</div>
+          <div className='badge badge-primary'> Gists: {public_gists}</div>
+          <div className='badge badge-primary'> Repos: {public_repos}</div>
+        </div>
       </Fragment>
     )
   }
